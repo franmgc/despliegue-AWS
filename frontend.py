e39cffc058d53dab434bd5719602d79ceefc43b2
franmgc <yo.fran.castro@gmail.com>
2023-10-22 22:51:54 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -186,6 +186,6 @@ def procesar_compra():
 
 # Make sure this we are executing this file
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=8080)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
 
